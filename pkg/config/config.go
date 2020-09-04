09548a1e2d80bb3438d64fe98bccbebd0f323c83
Vinicius Justino <justdgoncalves@gmail.com>
2020-09-03 21:33:28 -0300

    init config package

---
+++ pkg/config/config.go
@@ -0,0 +1,146 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	// HOMEPATH - application config file
+	HOMEPATH = ""
+	// APPHOME - folder with workspaces
+	APPHOME = ""
+)
+
+// Workspace - workspace configuration
+type Workspace struct {
+	Name    string `yaml:"name"`
+	Cluster string `yaml:"cluster"`
+	Region  string `yaml:"region"`
+}
+
+// Config - configuration map
+type Config struct {
+	homePath string
+	appHome  string
+
+	CurrentWorkspace     Workspace
+	CurrentWorkspaceName string `yaml:"workspace"`
+}
+
+// CreateWorkspace - write workspace file
+func CreateWorkspace(name, cluster, region string) error {
+	workspace := Workspace{
+		Name:    name,
+		Cluster: cluster,
+		Region:  region,
+	}
+
+	yaml, err := yaml.Marshal(&workspace)
+	fmt.Println(string(yaml))
+	if err != nil {
+		return err
+	}
+
+	yamlFile := fmt.Sprintf("%s/%s.yml", APPHOME, name)
+
+	f, err := os.Create(yamlFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.Write(yaml); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ChangeWorkspace - change current workspace
+func ChangeWorkspace(name string) error {
+	f, err := os.OpenFile(HOMEPATH, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	config := Config{
+		CurrentWorkspaceName: name,
+	}
+
+	yaml, err := yaml.Marshal(&config)
+	if err != nil {
+		return err
+	}
+
+	if _, err := f.Write(yaml); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getWorkspace(file string) (string, error) {
+	var conf Config
+	var workspace string
+
+	yamlFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		return workspace, err
+	}
+
+	if err := yaml.Unmarshal(yamlFile, &conf); err != nil {
+		return workspace, err
+	}
+
+	workspace = conf.CurrentWorkspaceName
+
+	return workspace, nil
+}
+
+func readClusterFile(file string) (Workspace, error) {
+	var workspace Workspace
+
+	yamlFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		return workspace, err
+	}
+
+	if err := yaml.Unmarshal(yamlFile, &workspace); err != nil {
+		return workspace, err
+	}
+
+	return workspace, nil
+}
+
+// NewConfig - create new config
+func NewConfig() *Config {
+	conf := &Config{
+		homePath: HOMEPATH,
+		appHome:  APPHOME,
+	}
+
+	// get current workspace
+	wkName, err := getWorkspace(conf.homePath)
+	if err != nil {
+		fmt.Printf("Unable to open workspace file reason: %s\n", err)
+		panic(err)
+	}
+	conf.CurrentWorkspaceName = wkName
+
+	wkFile := fmt.Sprintf("%s/%s.yml", conf.appHome, conf.CurrentWorkspaceName)
+
+	// add config
+	wkConf, err := readClusterFile(wkFile)
+	if err != nil {
+		fmt.Printf("Unable to use workspace reason: %s\n", err)
+		panic(err)
+	}
+	conf.CurrentWorkspace = wkConf
+
+	return conf
+}
