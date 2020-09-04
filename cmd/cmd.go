fb8629d2a2bab1bbae5630ce13d0d0f7ad614549
Vinicius Justino <justdgoncalves@gmail.com>
2020-09-03 21:48:32 -0300

    start cmd package

---
+++ cmd/cmd.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/projectops/ecsw/cmd/initcmd"
+	"github.com/projectops/ecsw/cmd/selectcmd"
+	"github.com/projectops/ecsw/cmd/showcmd"
+)
+
+// Runner -
+type Runner interface {
+	Init([]string) error
+	Run() error
+	Name() string
+}
+
+// Root -
+func Root(args []string) error {
+	if len(args) < 1 {
+		return errors.New("You must pass a subcommand ()")
+	}
+
+	subcommand := os.Args[1]
+
+	switch command := args[0]; command {
+	case "init":
+		cmds := []Runner{
+			initcmd.NewInitCmd(),
+		}
+
+		for _, cmd := range cmds {
+			if cmd.Name() == subcommand {
+				cmd.Init(os.Args[2:])
+				return cmd.Run()
+			}
+		}
+	case "select":
+		cmds := []Runner{
+			selectcmd.NewSelectCmd(),
+		}
+
+		for _, cmd := range cmds {
+			if cmd.Name() == subcommand {
+				cmd.Init(os.Args[2:])
+				return cmd.Run()
+			}
+		}
+	case "show":
+		cmds := []Runner{
+			showcmd.NewShowCmd(),
+		}
+
+		for _, cmd := range cmds {
+			if cmd.Name() == subcommand {
+				cmd.Init(os.Args[2:])
+				return cmd.Run()
+			}
+		}
+	default:
+		fmt.Println("Nothing!")
+	}
+
+	return fmt.Errorf("Unknown subcommand: %s", subcommand)
+}
