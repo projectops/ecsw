ad4a8b3b4a02729b926d3378750da7e220125218
Vinicius Justino <justdgoncalves@gmail.com>
2020-09-14 22:50:27 -0300

    test cmd

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/projectops/ecsw/cmd"
+)
+
+func main() {
+	if err := cmd.Root(os.Args[1:]); err != nil {
+		fmt.Println(err)
+	}
+}
